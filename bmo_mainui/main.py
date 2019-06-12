be74ea584001329261b1f6667bd0a2c42f43d15f
Hyoeun Lee <ggrr12345@naver.com>
2019-06-12 15:33:27 +0100

    main gui connect

---
+++ bmo_mainui/main.py
@@ -0,0 +1,99 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+import subprocess
+import random
+import time
+import os
+
+form_class1 = uic.loadUiType('main2.ui')[0]
+# form_class2 = uic.loadUiType('main.ui')[0]
+CurrentIndex = 0
+
+subprocess.Popen("googlesamples-assistant-hotword --project_id bmo-assistant-f4b98 --device_model_id bmo-assistant-f4b98-bmo-3x19ql", shell=True)
+
+class MyWindow(QMainWindow, form_class1):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle('BMO')
+        self.resize(480, 320)
+        self.center()
+        self.imagelist = ['image/bmo3.png', 'image/bmoangry.png', 'image/bmoeyeclose.png', 'image/bmoholy.png', 'image/bmosmile.png']
+        pixmap = QPixmap(self.imagelist[random.randint(0, 4)])
+        self.label.setPixmap(pixmap)
+        self.pushButton.clicked.connect(self.main_clicked)
+        self.pushButton_2.clicked.connect(self.add_clicked)
+        self.back_main.clicked.connect(self.back1_clicked)
+        self.back_manu.clicked.connect(self.back2_clicked)
+        self.setting.clicked.connect(self.setting_clicked)
+        # self.game1.clicked.connect(self.g1_clicked)
+        # self.game2.clicked.connect(self.g2_clicked)
+        self.listWidget.itemClicked.connect(self.game_clicked)
+        self.exit.clicked.connect(QCoreApplication.instance().quit)
+        self.restart.clicked.connect(self.restart_clicked)
+        self.sys_reboot.clicked.connect(self.reboot_clicked)
+        self.sys_shutdown.clicked.connect(self.shutdown_clicked)
+
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def keyPressEvent(self, e):
+        if(CurrentIndex == 0):
+            self.stackedWidget.setCurrentIndex(1)
+
+    def add_clicked(self):
+        fname = QFileDialog.getOpenFileName(self)
+        # fdir = QFileDialog.getExistingDirectory(self)
+        self.listWidget.addItem(fname[0])
+
+    def main_clicked(self):
+        self.stackedWidget.setCurrentIndex(1)
+        CurrentIndex = 1
+
+    def back1_clicked(self):
+        pixmap = QPixmap(self.imagelist[random.randint(0, 4)])
+        self.label.setPixmap(pixmap)
+        self.stackedWidget.setCurrentIndex(0)
+        CurrentIndex = 0
+
+    def back2_clicked(self):
+        self.stackedWidget.setCurrentIndex(1)
+        CurrentIndex = 1
+
+    def setting_clicked(self):
+        self.stackedWidget.setCurrentIndex(2)
+        CurrentIndex = 2
+
+    def game_clicked(self, item):
+        if(item.text()=="- game1.py"):
+            subprocess.call("python3"+" /home/pi/BMO_project/pygame/pygame_pyflying/pyflying.py", shell=True)
+        elif(item.text()=="- game2.py"):
+            subprocess.call("python3"+" /home/pi/BMO_project/bmoGame_project/game.py", shell=True)
+        else:
+            subprocess.call("python3"+" {}".format(item.text()), shell=True)
+            print("python3"+" {}".format(item.text()))
+     
+    def restart_clicked(self):
+        self.close()
+        subprocess.call("python3"+" main.py", shell=True)
+
+    def shutdown_clicked(self):
+        os.system('sudo shutdown now')
+
+    def reboot_clicked(self):
+        os.system("sudo reboot")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mywindow = MyWindow()
+    mywindow.showFullScreen()
+    sys.exit(app.exec_())
