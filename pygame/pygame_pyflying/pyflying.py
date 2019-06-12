4bc8e5edda86cd0f26ed3d1ab5fafd1eafa3a109
yrkr1349 <41564858+yrkr1349@users.noreply.github.com>
2019-06-12 21:30:01 +0900

    Update pyflying.py

--- pygame/pygame_pyflying/pyflying.py
+++ pygame/pygame_pyflying/pyflying.py
@@ -1,5 +1,5 @@
 import sys
-import pygame
+import pygame #pygame 라이브러리 import
 import os
 from pygame.locals import *
 import random
@@ -7,27 +7,31 @@ from time import sleep
 
 pygame.init()
 
+#게임의 폭, 높이 등 전체 크기 및 사용할 png 이미지 크기
 WHITE = (255,255,255)
 RED = (255,0,0)
-pad_width = 1024
-pad_height = 512
-background_width= 1024
+pad_width = 480
+pad_height = 320
+background_width= 480
 
-aircraft_width = 90
-aircraft_height = 55
+aircraft_width = 55
+aircraft_height = 31
 
-Shotship_width = 110
-Shotship_height = 67
+Shotship_width = 63
+Shotship_height = 25
 
-sonic1_width = 140
-sonic1_height = 60
-sonic2_width = 86
-sonic2_height = 60
+sonic1_width = 55
+sonic1_height = 41
+sonic2_width = 48
+sonic2_height = 36
 
+#_______________%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%___________________________
+#text 화면 출력
 def textObj(text,font):
     textSurface = font.render(text, True, RED)
     return textSurface, textSurface.get_rect()
 
+#text 화면 중앙에 출력
 def dispMessage(text):
     global gamepad
 
@@ -38,16 +42,17 @@ def dispMessage(text):
     pygame.display.update()
     sleep(2)
     runGame()
-
+# 적에게 공격당했을 때 호출
 def crash():
     global gamepad
     dispMessage('Crashed!')
 
-def drawObject(obj, x, y):
+def drawObject(obj, x, y): 
     global gamepad
     gamepad.blit(obj,(x,y))
-    
 
+#_______________%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%___________________________
+# 게임 실행
 def runGame():
     global gamepad, aircraft, clock, background1, background2
     global Shotship, sonics, bullet, boom
@@ -56,10 +61,10 @@ def runGame():
     boom_count = 0
 
     bullet_xy = []
-
+    #우주선 최초 위치 좌표 설정
     x = pad_width * 0.05
     y = pad_height * 0.8
-    y_change = 0
+    y_change = 0 #비행기 좌표 변화를 나타낼 
 
     background1_x = 0
     background2_x = background_width
@@ -78,13 +83,13 @@ def runGame():
             if event.type == pygame.QUIT:
                 crashed = True
 
-
+            #키를 눌렀을 때 우주선이 위아래로 5픽셀씩 이동
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     y_change = -5
                 elif event.key == pygame.K_DOWN:
                     y_change = 5
-
+                #ctrl를 누르면 총알 발사
                 elif event.key == pygame.K_LCTRL:
                     bullet_x = x + aircraft_width
                     bullet_y = y + aircraft_height/2
@@ -127,6 +132,7 @@ def runGame():
 
 
         #Shotship Position
+        # 적 우주선이 7픽셀씩 날아오도록 지정
         Shotship_x -= 7
         if Shotship_x <= 0:
             Shotship_x = pad_width
@@ -161,7 +167,7 @@ def runGame():
                         bullet_xy.remove(bxy)
                     except:
                         pass
-
+        #crash check
         if x + aircraft_width > Shotship_x:
             if ( y > Shotship_y and y < Shotship_y + Shotship_height) or \
             (y + aircraft_height > Shotship_y and y + aircraft_height < Shotship_y + Shotship_height):
@@ -182,9 +188,6 @@ def runGame():
             (y + aircraft_height > sonic_y and y + aircraft_height < sonic_y + sonic_height):
                 crash()
 
-#_______________%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%___________________________
-
-
         drawObject(aircraft,x,y)
 
 
@@ -194,7 +197,8 @@ def runGame():
 
         if not isShotship:  
             drawObject(Shotship,Shotship_x,Shotship_y)
-
+        #총알이 적을 명중하지 않았을 때 적 우주선 새로 화면에 갱신
+        #명중시 폭발 + 새로운 적 우주선 
         else:
             drawObject(boom, Shotship_x, Shotship_y)
             boom_count += 1
@@ -213,7 +217,8 @@ def runGame():
 
     pygame.quit()
     quit()
-
+#_______________%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%___________________________
+# 게임 초기화하고 시작
 def initGame():
     global gamepad, aircraft, clock, background1, background2
     global Shotship, sonics, bullet, boom, sonic1, sonic2
@@ -222,11 +227,11 @@ def initGame():
 
     pygame.init()
     gamepad = pygame.display.set_mode((pad_width, pad_height), FULLSCREEN)
-    pygame.display.set_caption('pyflying')
+    pygame.display.set_caption('Shooting game') #game title
 
     aircraft = pygame.image.load('/home/pi/BMO_project/pygame/image/spaceship.png')
     background1 = pygame.image.load("/home/pi/BMO_project/pygame/image/fly_background.png")
-    background2 = background1.copy()
+    background2 = background1.copy() #배경이 움직이도록 이미지 복사본을 원본 다음으로 좌표 지정
 
     Shotship = pygame.image.load("/home/pi/BMO_project/pygame/image/Shotship.png")
     
@@ -242,7 +247,7 @@ def initGame():
 
     bullet = pygame.image.load("/home/pi/BMO_project/pygame/image/bullet.png")
 
-    clock = pygame.time.Clock()
+    clock = pygame.time.Clock() #게임 초당 프레임 설정을 위한 Clock
     runGame()
     
     
